05c88ed9e06a6ef39f33e58718f5ffa62e36fb22
jjanczynska <joaxjan@gmail.com>
2023-10-31 13:34:05 +0000

    Update Allowed_hosts in settings.py

--- surf_center/surf_center/settings.py
+++ surf_center/surf_center/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-)z1m_&$8s-e!+71un169r4l_hogbima#^2)w3s%t1vz5l$=5w7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-jjanczynska-surfcenter-8tquorjvs86.ws-eu105.gitpod.io']
 
 
 # Application definition
