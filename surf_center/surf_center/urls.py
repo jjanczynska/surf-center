32782c4596bd3bfd4d46542a6429a6663306bbd3
jjanczynska <joaxjan@gmail.com>
2023-11-02 17:45:34 +0000

    Add home app and templates

--- surf_center/surf_center/urls.py
+++ surf_center/surf_center/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from home.views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path('', index, name='home'),
+   
 ]
+
