cd3a742a074520416c2633c521afe2121e8eb771
jjanczynska <joaxjan@gmail.com>
2023-11-06 18:53:33 +0000

    Refactor the account and shopping basket to include links to account registert, logout and superuser

--- surf_center/home/urls.py
+++ surf_center/home/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from home.views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),
+    path('', index, name='home'),
+   
 ]
