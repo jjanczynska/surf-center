fcb3692e82d2fc83a05e8f5f10edfe8562dfdc3b
jjanczynska <joaxjan@gmail.com>
2023-11-19 18:38:17 +0000

    Add templates for products and services

--- surf_center/product/urls.py
+++ surf_center/product/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.all_products, name='all_products'),
     path('surfing-equipment/', views.surfing_equipment, name='surfing_equipment'),
     path('lessons/', views.lessons, name='lessons'),
-    path(special-offers/, views.special_offers, name='special_offers'),
+    path('special-offers/', views.special_offers, name='special_offers'),
 ]
