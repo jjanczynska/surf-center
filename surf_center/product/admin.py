482280cb8baf0321c9b2f2f4be055ae6e3492752
jjanczynska <joaxjan@gmail.com>
2023-11-19 17:34:56 +0000

    Add functional admin panels for Category, Product and Service model

--- surf_center/product/admin.py
+++ surf_center/product/admin.py
@@ -1,6 +1,38 @@
 from django.contrib import admin
 from .models import Category, Product, Service
 
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Service)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku')
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+        'type',
+        'time_slots',
+        'date',
+        'max_participants',
+        'booked',
+    )
+
+    list_filter = ('type', 'date', 'booked',)
+    search_fields = ('category__name', 'date',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'display_name',
+        'name',
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Service, ServiceAdmin)
