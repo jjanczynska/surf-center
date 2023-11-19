9b0270b46b0a187e288dc2592798bee0c597b5bc
jjanczynska <joaxjan@gmail.com>
2023-11-19 18:28:18 +0000

    Add product urls and update main urls

---
+++ surf_center/product/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('', views.all_products, name='all_products'),
+    path('surfing-equipment/', views.surfing_equipment, name='surfing_equipment'),
+    path('lessons/', views.lessons, name='lessons'),
+    path(special-offers/, views.special_offers, name='special_offers'),
+]
