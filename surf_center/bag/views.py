8ea4a68512d5ec851fe7a1b2810ffadb4ed0545c
jjanczynska <joaxjan@gmail.com>
2023-11-27 21:31:18 +0000

    Add update and remove products and services from shopping bag, adjust bag template

--- surf_center/bag/views.py
+++ surf_center/bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.http import HttpResponse
 from product.models import Product, Service, LessonSchedule
 from decimal import Decimal
@@ -64,4 +64,78 @@ def add_to_bag(request, item_id, item_type):
     return redirect(redirect_url)
 
 
+def update_bag(request, item_id, item_type):
+    """Update the quantity of the specified product"""
+
+    bag = request.session.get('bag', {'products': {}, 'lessons': {}})
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['size']
+
+    if item_type == 'product':
+        quantity = int(request.POST.get('quantity', 1))
+        product = get_object_or_404(Product, pk=item_id)
+
+        if size:
+            if quantity > 0:
+                    bag['products'][item_id]['items_by_size'] = {size: quantity}
+            else:
+                del bag['products'][item_id]['items_by_size'][size]
+        else:
+            if quantity > 0:
+                    bag['products'][item_id] = quantity
+            else:
+                bag.pop[item_id]
+
+    elif item_type == 'lesson':
+        quantity = int(request.POST.get('quantity', 1))
+        lesson = get_object_or_404(Service, pk=item_id)
+        selected_date = request.POST.get('date')
+        selected_time_slot = request.POST.get('time_slot')
+
+        lesson_key = f"{selected_date}_{selected_time_slot}"
+        if lesson_key not in bag['lessons']:
+            bag['lessons'][lesson_key] = {
+                'item_id': item_id,
+                'quantity': quantity,
+                'details': {
+                    'date': selected_date,
+                    'time_slot': selected_time_slot,
+                    'total_price': quantity * lesson.price_per_participant
+                }
+            }
+        
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+def remove_from_bag(request, item_id):
+    """Remove the specified product or lesson from the shopping bag"""
+
+    try:
+        bag = request.session.get('bag', {'products': {}, 'lessons': {}})
+        item_type = request.POST.get('item_type')
+
+        if item_type == 'product':
+            size = request.POST.get('product_size', None)
+            product = get_object_or_404(Product, pk=item_id)
+
+            if size:
+                del bag['products'][item_id]['items_by_size'][size]
+                if not bag['products'][item_id]['items_by_size']:
+                    del bag['products'][item_id]
+            else:
+                del bag['products'][item_id]
+
+        elif item_type == 'lesson':
+            lesson = get_object_or_404(Service, pk=item_id)
+            selected_date = request.POST.get('date', None)
+            selected_time_slot = request.POST.get('time_slot', None)
+            lesson_key = f"{selected_date}_{selected_time_slot}"
+            del bag['lessons'][lesson_key]
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
 
