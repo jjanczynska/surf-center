9035fd8d2795ee898ab538d60285c75fe74ddff2
jjanczynska <joaxjan@gmail.com>
2023-11-28 20:16:48 +0000

    Add checkout to admin functionality

--- surf_center/checkout/admin.py
+++ surf_center/checkout/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Order, ProductsLineItem, LessonLineItem
 
-# Register your models here.
+class ProductsLineItemAdminInline(admin.TabularInline):
+    model = ProductsLineItem
+    readonly_fields = ('lineitem_total',)
+
+class LessonLineItemAdminInline(admin.TabularInline):
+    model = LessonLineItem
+    readonly_fields = ('lineitem_total',)
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (ProductsLineItemAdminInline, LessonLineItemAdminInline)
+
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total',)
+
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
+
+
