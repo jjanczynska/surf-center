5ddb383cde497922416f19f7f33e81964630f063
jjanczynska <joaxjan@gmail.com>
2023-11-28 19:31:22 +0000

    Update and migrate models in checkout app

--- surf_center/checkout/models.py
+++ surf_center/checkout/models.py
@@ -24,8 +24,32 @@ class Order(models.Model):
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
 
-class OrderLineItem(models.Model):
+    def update_total(self):
+        """
+        Update grand total each time a line item is added,
+        accounting for delivery costs.
+        """
+        product_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        lesson_total = self.lessonitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = product_total + lesson_total
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
+    def save(self, *args, **kwargs):
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+class ProductsLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
