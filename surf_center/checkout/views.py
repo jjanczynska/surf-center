68414ae9717c2f2109edf205a55689e5778c9a46
jjanczynska <joaxjan@gmail.com>
2023-11-29 20:40:45 +0000

    Update checkout view and add checkout_success view

--- surf_center/checkout/views.py
+++ surf_center/checkout/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
+from .models import Order, ProductsLineItem, LessonLineItem
+from product.models import Product, Service
 from bag.contexts import bag_contents
 
 import stripe
@@ -11,33 +13,109 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    bag = request.session.get('bag', {})
-    if not bag:
-        messages.error(request, "There's nothing in your bag at the moment")
-        return redirect(reverse('product'))
+    if request.method == 'POST':
+        bag = request.session.get('bag', {})
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save(commit=False)
+            order.original_bag = json.dumps(bag)
+            order.stripe_pid = request.POST.get('client_secret').split('_secret')[0]
+            order.save()
 
-    current_bag = bag_contents(request)
-    total = current_bag['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-    amount=stripe_total,
-    currency=settings.STRIPE_CURRENCY,
-    )
+            for item_id, item_data in bag.items():
+                try:
+                    if 'product' in item_data:
+                        product = Product.objects.get(id=item_id)
+                        for size, quantity in item_data['items_by_size'].items():
+                            order_line_item = ProductsLineItem(
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                                product_size=size,
+                            )
+                            order_line_item.save()
+                    if 'lesson' in item_data:
+                        lesson = Service.objects.get(id=item_id)
+                        order_line_item = LessonLineItem(
+                            order=order,
+                            service=lesson,
+                            quantity=item_data['quantity'],
+                            date=item_data['details']['date'],
+                            time_slot=item_data['details']['time_slot'],
+                        )
+                        order_line_item.save()
+                except (Product.DoesNotExist, Service.DoesNotExist):
+                    messages.error(request, (
+                        "One of the items wasn't found in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_bag'))
 
-    order_form = OrderForm()
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+    else:
+        bag = request.session.get('bag', {})
+        if not bag:
+            messages.error(request, "There's nothing in your bag at the moment")
+            return redirect(reverse('product'))
 
-    if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing. \
-            Did you forget to set it in your environment?')
-    
-    context =  bag_contents(request)  
-    context = {
-        'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
-    }
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        order_form = OrderForm()
+
+        if not stripe_public_key:
+            messages.warning(request, 'Stripe public key is missing. \
+                Did you forget to set it in your environment?')
         
-   
+        context =  bag_contents(request)  
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+        }
+            
     
-    return render(request, 'checkout/checkout.html', context)
+        
+        return render(request, 'checkout/checkout.html', context)
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed! \
+        Your surfing order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'bag' in request.session:
+        del request.session['bag']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+    }
+
+    return render(request, template, context)
