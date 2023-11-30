234fbd5413db7556a8e1f0eb52840a212488caa6
jjanczynska <joaxjan@gmail.com>
2023-11-30 13:39:12 +0000

    Add checkout caching data in payment intent

--- surf_center/checkout/urls.py
+++ surf_center/checkout/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout-success/<order_number>', views.checkout_success, name='checkout_success'),
+    path('cache-checkout-data/', views.cache_checkout_data, name='cache_checkout_data'),
+    path('wh/', webhook, name='webhook'),
 ]
