60043086235559c5a06e0128b0b6819dd441f0dd
jjanczynska <joaxjan@gmail.com>
2023-12-01 14:23:20 +0000

    Add ability to send confirmation emails

--- surf_center/surf_center/settings.py
+++ surf_center/surf_center/settings.py
@@ -195,5 +195,6 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    DEFAULT_FROM_EMAIL = 'surfcenter@example.com'
 
 
