7701503fb315125892703c788bd22d5877348cfc
jjanczynska <joaxjan@gmail.com>
2023-12-03 13:54:29 +0000

    Correct typo

--- surf_center/settings.py
+++ surf_center/settings.py
@@ -188,7 +188,7 @@ if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'surfcenter'
     AWS_S3_REGION_NAME = 'Europe (Stockholm) eu-north-1'
     AWS_ACCESS_KEY_ID = os.enviro.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.enviro.get('AWS_SECRET_ACCESS_KEY')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and Media Files 
