c110f4eb92dead27d61edcb823f4835cee5fc641
jjanczynska <joaxjan@gmail.com>
2023-12-03 12:36:40 +0000

    Reorganize folder structure for deployment to heroku and update Procfile

--- surf_center/settings.py
+++ surf_center/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = 'django-insecure-)z1m_&$8s-e!+71un169r4l_hogbima#^2)w3s%t1vz5l$=5w7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://surf-center-and-shop-5e23c52a2d1d.herokuapp.com/, 8000-jjanczynska-surfcenter-8tquorjvs86.ws-eu105.gitpod.io', '8000-jjanczynska-surfcenter-8tquorjvs86.ws-eu106.gitpod.io']
+ALLOWED_HOSTS = [
+    'https://surf-center-and-shop-5e23c52a2d1d.herokuapp.com/', 
+    '8000-jjanczynska-surfcenter-8tquorjvs86.ws-eu105.gitpod.io', 
+    '8000-jjanczynska-surfcenter-8tquorjvs86.ws-eu106.gitpod.io',
+]
 
 
 # Application definition
