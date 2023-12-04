943f6fe064d7d1d4238c47a4f02ae96b7f26f25f
jjanczynska <joaxjan@gmail.com>
2023-12-04 11:25:53 +0000

    Add heroku deployed app to allowed host

--- surf_center/settings.py
+++ surf_center/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)z1m_&$8s-e!+71un169r4l_hogbima#^2)w3s%t1vz5l$=5w7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    'https://surf-center-and-shop-5e23c52a2d1d.herokuapp.com/', 
+    'surf-center-and-shop-5e23c52a2d1d.herokuapp.com', 
     '8000-jjanczynska-surfcenter-8tquorjvs86.ws-eu105.gitpod.io', 
     '8000-jjanczynska-surfcenter-8tquorjvs86.ws-eu106.gitpod.io',
 ]
